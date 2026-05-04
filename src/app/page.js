de1a68bbe448539b0bd99e681ccbf4a0b1a47102
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-05-05 00:45:40 +0600

    Summer Care Tips Section is done with mobile Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
 import PopularProducts from "@/components/PopularProducts";
+import SummerCareTips from "@/components/SummerCareTips";
 
 
 export default function Home() {
   return <div>
       <Banner />
       <PopularProducts />
+      <SummerCareTips />
     </div>
   
 }
