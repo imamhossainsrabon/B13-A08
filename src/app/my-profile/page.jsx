3a2b2581c148f48f8e7e930c8703272754869d86
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-05-06 05:42:55 +0600

    deployment issue fixed

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const MyProfilePage = async () => {
@@ -15,6 +16,8 @@ const MyProfilePage = async () => {
     <div className="max-w-[500px] mx-auto mt-10 p-6 rounded-2xl shadow">
       <h1 className="text-2xl font-bold mb-4">My Profile</h1>
 
+      <div className="space-y-5 text-center text-2xl">
+        <Image src={session.user.image} width={1000} height={1000} alt={session.user.name} className="w-20 h-20 object-cover aspect-square mx-auto rounded-full"></Image>
       <p>
         <span className="font-semibold">Name:</span> {session.user.name}
       </p>
@@ -22,6 +25,7 @@ const MyProfilePage = async () => {
       <p>
         <span className="font-semibold">Email:</span> {session.user.email}
       </p>
+      </div>
     </div>
   );
 };
